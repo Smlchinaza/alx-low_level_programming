416d590dac479ef610fffdfec6c6821e31b42d98
Smlchinaza <samchuks898@gmail.com>
2023-02-19 12:25:02 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	write(1, "and that peoce of art is useful \" -Dera Korper, 2015-10-19\n");
+	write(1, "and that piece of art is useful \" - Dera Korper, 2015-10-19\n");
 	return (1);
 }
