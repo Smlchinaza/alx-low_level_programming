a1648c1258e19389af5133f76cebbfd0b30e0028
Smlchinaza <samchuks898@gmail.com>
2023-02-19 12:17:13 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry
+ * Return: 1
+ */
+int main(void)
+{
+	write(1, "and that peoce of art is useful \" -Dera Korper, 2015-10-19\n", 59);
+	return (1);
+}
